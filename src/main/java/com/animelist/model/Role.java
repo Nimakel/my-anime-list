4e8f9e029a1851abef8a1d72ef23021bde111f99
Nimakel <maxim.yashyn@gmail.com>
2023-07-12 17:20:12 +0300

    [MODEL] implemented Role model

--- src/main/java/com/animelist/model/Role.java
+++ src/main/java/com/animelist/model/Role.java
@@ -1,4 +1,27 @@
 package com.animelist.model;
 
-public enum Role {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role_name")
+    @Enumerated(value = EnumType.STRING)
+    private RoleName roleName;
+
+    public enum RoleName {
+        ADMIN, USER;
+    }
 }
