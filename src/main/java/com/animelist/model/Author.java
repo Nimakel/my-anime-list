292892a7652762000f4ebcef6e2f34a44b27604d
Nimakel <maxim.yashyn@gmail.com>
2023-07-12 17:20:30 +0300

    [MODEL] implemented Author model

--- src/main/java/com/animelist/model/Author.java
+++ src/main/java/com/animelist/model/Author.java
@@ -1,4 +1,20 @@
 package com.animelist.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "author_name")
+    private String name;
 }
