6687712a9660c9076815148094523dce949e76d4
Nimakel <maxim.yashyn@gmail.com>
2023-07-12 17:20:20 +0300

    [MODEL] implemented Order model

--- src/main/java/com/animelist/model/Order.java
+++ src/main/java/com/animelist/model/Order.java
@@ -1,4 +1,32 @@
 package com.animelist.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "anime_id")
+    private Anime anime;
+    @Column(name = "user_mark")
+    private Integer mark;
+    @Column(name = "type_of_list")
+    private Integer list;
+    @Column(name = "changes_date")
+    private LocalDateTime changesDate;
 }
