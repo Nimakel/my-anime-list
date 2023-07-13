36474d5bdb4638baba0116cd8912ad771c28c20a
Nimakel <maxim.yashyn@gmail.com>
2023-07-13 11:47:15 +0300

    [DTO] implemented UserRequestDto class

--- src/main/java/com/animelist/dto/request/UserRequestDto.java
+++ src/main/java/com/animelist/dto/request/UserRequestDto.java
@@ -1,5 +1,20 @@
 package com.animelist.dto.request;
 
-public class UserRequestDto {
+import com.animelist.lib.FieldsValueMatch;
+import com.animelist.lib.ValidLogin;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
 
+@FieldsValueMatch(
+        field = "password",
+        fieldMatch = "repeatPassword",
+        message = "Password do not match!"
+)
+@Getter
+public class UserRequestDto {
+    @ValidLogin
+    private String login;
+    @Size(min = 8, max = 40)
+    private String password;
+    private String repeatPassword;
 }
