22ff7b4e1ae4f7b609928cad3aef5184b0477191
Nimakel <maxim.yashyn@gmail.com>
2023-07-13 11:55:19 +0300

    [SECURITY] refactoring code

--- src/main/java/com/animelist/lib/FieldsValueMatchValidator.java
+++ src/main/java/com/animelist/lib/FieldsValueMatchValidator.java
@@ -1,9 +1,8 @@
 package com.animelist.lib;
 
+import org.springframework.beans.BeanWrapperImpl;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.springframework.beans.BeanWrapperImpl;
-
 import java.util.Objects;
 
 public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValueMatch, Object> {
