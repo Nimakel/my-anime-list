af5ad3073e31b0b6bd7b9a76eee65452b229863b
Nimakel <maxim.yashyn@gmail.com>
2023-07-13 20:22:50 +0300

    [SECURITY] implemented CustomUserDetailsService

--- src/main/java/com/animelist/security/CustomUserDetailsService.java
+++ src/main/java/com/animelist/security/CustomUserDetailsService.java
@@ -1,12 +1,29 @@
 package com.animelist.security;
 
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+import com.animelist.model.User;
+import com.animelist.service.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userService.findByLogin(username).orElseThrow(() ->
+                new RuntimeException("Can't find user by login: " + username));
+        org.springframework.security.core.userdetails.User.UserBuilder builder
+                = withUsername(username);
+        builder.password(user.getPassword());
+        builder.authorities(user.getRoles().stream()
+                .map(r -> r.getRoleName().name()).toArray(String[]::new));
+        return builder.build();
     }
 }
