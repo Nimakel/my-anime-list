41c46817f1761a19b036ce0f6e15e103ba28eb4b
Nimakel <maxim.yashyn@gmail.com>
2023-07-13 13:28:38 +0300

    [REPOSITORY] created StudioRepository

---
+++ src/main/java/com/animelist/repository/StudioRepository.java
@@ -0,0 +1,9 @@
+package com.animelist.repository;
+
+import com.animelist.model.Studio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudioRepository extends JpaRepository<Studio, Long> {
+}
